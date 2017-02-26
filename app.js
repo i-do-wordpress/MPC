3d69b9ce122f3ab6c4cc61493d70918ce28b82ba
i-do-wordpress <i-do-wordpress@wp.pl>
2017-02-26 17:53:19 +0000

    2

--- app.js
+++ app.js
@@ -12,6 +12,8 @@
           var text = angular.element(document).find('#lessIsMoreText');
           var view = angular.element(document).find('#viewWrapper')
           var wrapper = text.parent().parent();
+          
+          /*
           if(text.length && view.length && wrapper.length){
             text.fadeIn(3000).delay(2000).fadeOut(1000);
             wrapper.delay(5000).fadeOut(2000, function(){
@@ -19,6 +21,21 @@
               clearInterval(stop)
             });
           }
+          */
+          if(text.length && view.length && wrapper.length){
+            text.fadeIn(3).delay(2).fadeOut(1);
+            wrapper.delay(5).fadeOut(2, function(){
+              view.fadeIn(8);          
+              clearInterval(stop)
+            });
+          }
+          
+          
+          
+          
+          
+          
+          
         }, 1000/60);
       };
       preloader();
@@ -71,14 +88,38 @@
       
       pix.controller('CtrlRoot', CtrlRoot);
       
-      CtrlRoot.$inject = [];
+      CtrlRoot.$inject = ['$http'];
       
-      function CtrlRoot(){
+      function CtrlRoot($http){
         
         var self = this;
         
+        self.checking = false;
         
+        self.pixel = {};
+        self.pixel.pass = '';
         
+        self.auth = function(form){
+          
+          if(form.$valid && self.pixel.pass){
+            
+            self.checking = true;
+            
+            $http.post("https://useless-space.comli.com/auth.php", self.pixel.pass).then(function(response){
+              
+              console.log(response);
+              
+              
+            }, function(response){
+              
+              console.log(response);
+              
+            });
+            
+            
+            
+          }
+        }
         
         
         
